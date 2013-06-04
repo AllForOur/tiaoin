01fd0193a634bc975618e8fcf1439719e3eef202
your117 <sky17yang@sina.com>
2013-06-04 18:22:29 +0800

    update

---
+++ tiaoin-crawl/tiaoin-crawl-core/src/test/java/com/tiaoin/crawl/core/StartupTest.java
@@ -0,0 +1,12 @@
+package com.tiaoin.crawl.core;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath*:**/spring-core-service.xml")
+public class StartupTest extends AbstractJUnit4SpringContextTests{
+
+}
